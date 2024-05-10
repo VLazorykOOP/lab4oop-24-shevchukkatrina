e1e2a499dd9f686f1019f95a4e7f42dca9ab14f5
Kateryna Shevchuk <shevchuk.kateryna@chnu.edu.ua>
2024-05-10 12:46:58 +0300

    complite task 1

--- OOPLab4T/OOPLab4T.cpp
+++ OOPLab4T/OOPLab4T.cpp
@@ -1,24 +1,91 @@
-﻿ // OOPLab4T.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №4. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <stdexcept>
 
+// завдання 1
+class VectorLong {
+private:
+    long* ptr;
+    int size;
+    mutable int codeError; // Зробити codeError змінною
 
-#include <iostream>
+public:
+    static int objectCount;
 
-// Ваші файли загловки 
-//
-#include "Lab4Exmaple.h"
-int main()
-{
-    std::cout << " Lab #4  task chain  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+    // Конструктор за замовчуванням
+    VectorLong() : ptr(nullptr), size(0), codeError(0) {
+        objectCount++;
+    }
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
+    // Конструктор з одним параметром - розмір вектора
+    VectorLong(int s) : ptr(new long[s]), size(s), codeError(0) {
+        for (int i = 0; i < size; ++i)
+            ptr[i] = 0;
+        objectCount++;
+    }
 
-}
+    // Деструктор
+    ~VectorLong() {
+        delete[] ptr;
+        objectCount--;
+    }
+
+    // Перегрузка оператора додавання +
+    VectorLong operator+(const VectorLong& other) const {
+        if (size != other.size) {
+            codeError = 1; // Змінено на змінну codeError
+            return *this; // Повертаємо не змінений вектор у разі різних розмірів
+        }
+        VectorLong result(size);
+        for (int i = 0; i < size; ++i)
+            result.ptr[i] = ptr[i] + other.ptr[i];
+        return result;
+    }
+
+    // Перегрузка оператора віднімання -
+    VectorLong operator-(const VectorLong& other) const {
+        if (size != other.size) {
+            codeError = 1; // Змінено на змінну codeError
+            return *this; // Повертаємо не змінений вектор у разі різних розмірів
+        }
+        VectorLong result(size);
+        for (int i = 0; i < size; ++i)
+            result.ptr[i] = ptr[i] - other.ptr[i];
+        return result;
+    }
+
+    // Перегрузка оператора множення *
+    VectorLong operator*(long scalar) const {
+        VectorLong result(size);
+        for (int i = 0; i < size; ++i)
+            result.ptr[i] = ptr[i] * scalar;
+        return result;
+    }
+
+    // Перегрузка оператора менше рівне <=
+    bool operator<=(const VectorLong& other) const {
+        for (int i = 0; i < size; ++i) {
+            if (ptr[i] > other.ptr[i])
+                return false;
+        }
+        return true;
+    }
+
+    // Перегрузка оператора введення >>
+    friend std::istream& operator>>(std::istream& in, VectorLong& vector) {
+        for (int i = 0; i < vector.size; ++i)
+            in >> vector.ptr[i];
+        return in;
+    }
+
+    // Перегрузка оператора виведення <<
+    friend std::ostream& operator<<(std::ostream& out, const VectorLong& vector) {
+        for (int i = 0; i < vector.size; ++i)
+            out << vector.ptr[i] << " ";
+        return out;
+    }
+};
+
+int VectorLong::objectCount = 0;
+
