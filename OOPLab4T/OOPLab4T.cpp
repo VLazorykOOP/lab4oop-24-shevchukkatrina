b22898a1ed45e3e388f5e5b3da76e8ef7b13f1fc
Kateryna Shevchuk <shevchuk.kateryna@chnu.edu.ua>
2024-05-10 12:47:24 +0300

    complite task 2

--- OOPLab4T/OOPLab4T.cpp
+++ OOPLab4T/OOPLab4T.cpp
@@ -89,3 +89,51 @@ public:
 
 int VectorLong::objectCount = 0;
 
+// завдання 2
+
+
+class Association {
+private:
+    std::map<std::string, std::map<std::string, std::string>> data;
+
+public:
+    std::map<std::string, std::string>& operator[](const std::string& key) {
+        return data[key];
+    }
+
+    std::string& operator()(const std::string& key1, const std::string& key2) {
+        return data[key1][key2];
+    }
+
+    const std::map<std::string, std::string>& operator[](const std::string& key) const {
+        auto it = data.find(key);
+        if (it != data.end()) {
+            return it->second;
+        }
+        else {
+            throw std::out_of_range("Key not found");
+        }
+    }
+
+    const std::string& operator()(const std::string& key1, const std::string& key2) const {
+        auto it1 = data.find(key1);
+        if (it1 != data.end()) {
+            auto it2 = it1->second.find(key2);
+            if (it2 != it1->second.end()) {
+                return it2->second;
+            }
+        }
+        throw std::out_of_range("Keys not found");
+    }
+
+    void print() const {
+        for (const auto& entry : data) {
+            std::cout << "Email: " << entry.first << std::endl;
+            for (const auto& info : entry.second) {
+                std::cout << info.first << ": " << info.second << std::endl;
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
